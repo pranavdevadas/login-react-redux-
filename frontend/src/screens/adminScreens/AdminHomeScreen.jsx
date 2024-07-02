c1d97921f7bbfd16fca98e5f65fb2dbf9934bdac
pranavdevadas <pranavdevadas2@gmail.com>
2024-07-02 15:24:53 +0530

    Initial commit: added project files

---
+++ frontend/src/screens/adminScreens/AdminHomeScreen.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AdminHero from "../../components/adminComponents/AdminHero";
+
+function AdminHomeScreen() {
+  return <AdminHero />;
+}
+
+export default AdminHomeScreen;
